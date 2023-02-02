11792f85170c65725af9212e8fc16422a74d5070
Nyanda Freddy <87903153+nyandajr@users.noreply.github.com>
2023-02-02 22:13:15 +0300

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from predict_page import show_predict_page
 from visualization_page import show_visualization_page
+import sklearn
 
 
 page = st.sidebar.selectbox("Select App or Visualize side bar", ("Visualize", "predict"))
